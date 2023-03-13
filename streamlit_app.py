0b246e7e546b0621dc8d7536a4febe9184c5ebbb
Claire Huang <42448888+clairehhw@users.noreply.github.com>
2023-03-13 13:28:40 -0700

    Update streamlit_app.py
    updated emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,10 +2,10 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('🥣 Breakfast Favorites')
-streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.header('Breakfast Favorites')
+streamlit.text('\N{bowl with spoon} Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Acocado Toast')
 
-streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+streamlit.header('🍌\N{strawberry} Build Your Own Fruit Smoothie 🥝🍇')
