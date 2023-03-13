67a0d31c1d8a55a854136ec37d42453acf1bb90c
Claire Huang <42448888+clairehhw@users.noreply.github.com>
2023-03-13 13:46:02 -0700

    Update streamlit_app.py
    make dataframe show only selected fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Mom's New Healthy Diner')
+streamlit.title("My Mom's New Healthy Diner")
 
 streamlit.header('Breakfast Favorites')
 streamlit.text('🍳 Omega 3 & Blueberry Oatmeal')
@@ -15,6 +15,7 @@ my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.co
 my_fruit_list = my_fruit_list.set_index('Fruit')
 
 # make a list so that people can pick the kinds of fruit they want to include
-streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado, Strawberries'])
+fruits_selected = streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index),['Avocado', 'Strawberries'])
+fruits_to_show = my_fruit_list.loc[fruits_selected]
 
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)
