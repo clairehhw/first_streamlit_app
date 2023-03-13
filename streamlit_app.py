89dfb2878514ae55145981a3da0eddfb1b00213d
Claire Huang <42448888+clairehhw@users.noreply.github.com>
2023-03-13 13:40:35 -0700

    Update streamlit_app.py
    added fruit selections

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,4 +12,8 @@ streamlit.header('🍌\N{strawberry} Build Your Own Fruit Smoothie 🥝🍇')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+
+# make a list so that people can pick the kinds of fruit they want to include
+streamlit.multiselect("Pick some fruits:",list(my_fruit_list.index))
+
 streamlit.dataframe(my_fruit_list)
